9678d5459b3c86f4a7a4f65b1b3cdcfd6b9771b2
Ujjawal Rachhoya <81584747+noobtuber20103152@users.noreply.github.com>
2023-12-18 18:32:19 +0530

    frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -187,7 +187,7 @@ export default function Home() {
                     type="+[1-9]"
                     value={data.contactNumber}
                     className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="xxxx-xxxx-xxxx-xxxx"
+                    placeholder="Enter you contact no eg. 8890******"
                   />
                   <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                     <svg
