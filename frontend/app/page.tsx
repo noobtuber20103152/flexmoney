e92d4247a7b9a7f14ce920982e1d816f3b011d6b
Ujjawal Rachhoya <81584747+noobtuber20103152@users.noreply.github.com>
2023-12-23 12:55:57 +0530

    change indian flag

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -351,7 +351,7 @@ export default function Home() {
                   <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                     <img
                       className="h-4 w-4 object-contain"
-                      src="https://flagpack.xyz/_nuxt/4c829b6c0131de7162790d2f897a90fd.svg"
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_India.png/2560px-Flag_of_India.png"
                       alt=""
                     />
                   </div>
